11fd610ebd91a27352aad3a3b7d83af05e68b237
isaiahsnyder-programming <isaiahsnyder.programming@gmail.com>
2022-05-06 12:20:52 -0600

    ngIf/ngFor 101

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  courses = [];
+  courses = [1, 2];
+  viewMode = 'somethingElse';
+
+  courseObjects = [
+    { id: 1, name: 'course 1'},
+    { id: 2, name: 'course 2'},
+    { id: 3, name: 'course 3'}
+  ];
+
+  onAdd() {
+    this.courseObjects.push({ id: 4, name: 'course 4'});
+  }
+
+  onRemove(course) {
+    let index = this.courses.indexOf(course);
+    this.courseObjects.splice(index, 1);
+  }
+
+  onChange(course) {
+    course.name = 'UPDATED COURSE';
+  }
 }
